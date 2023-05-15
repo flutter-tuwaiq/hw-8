ecd620d24686faa65214a1ee7dce294e45f0d6e3
herz1291 <hheerrzz1991@gmail.com>
2023-05-16 00:51:37 +0300

    Amnah Alherz

--- bin/Handlers/ImageHandler.dart
+++ bin/Handlers/ImageHandler.dart
@@ -5,12 +5,13 @@ import 'package:http_parser/http_parser.dart';
 
 Future<Response> imageHandler(Request req) async {
   try {
+    final contentType = MediaType.parse(req.headers['Content-Type']!);
+
     final body = await req.read().expand((element) => element).toList();
-    final file = File('bin/images/image${DateTime.now()}.png');
+    final file = File(
+        'bin/images/image${DateTime.now()}.${contentType.mimeType.replaceAll('image/', '')}');
 
     await file.writeAsBytes(body);
-
-    
 
     return Response.ok('Upload image successfully');
   } catch (e) {}
--- bin/server.dart
+++ bin/server.dart
@@ -18,7 +18,10 @@ Future<HttpServer> createServer() async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
-  final pipline = Pipeline().addMiddleware(logRequests()).addMiddleware(checkHeaderJson).addHandler(BaseRoute().handler);
+  final pipline = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(checkHeaderJson)
+      .addHandler(BaseRoute().handler);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
