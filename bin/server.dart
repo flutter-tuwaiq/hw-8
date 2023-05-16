415f16932254e59b6ef063628d60975e0a2edc96
Mohammed <dev.mjhh@gmail.com>
2023-05-16 04:11:23 +0300

    Edit: Add Middleware

--- bin/server.dart
+++ bin/server.dart
@@ -13,8 +13,8 @@ void main() {
 Future<HttpServer> createServer() async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
-
-  final pipline = Pipeline().addHandler(BaseRoute().handler);
+  
+  final pipline = Pipeline().addMiddleware(logRequests()).addHandler(BaseRoute().handler);
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(pipline, ip, port);
